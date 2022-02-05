c6b402c1ee3af5dabd85b836c071089926d749a4
alireza kargar <imanesche@gmail.com>
2022-02-05 22:38:51 +0330

    declaration a variable in go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alirezakargar1380/different_ways_to_set_variables_in_golang
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// keyword variable_name type
+	var car_name string = "ford"
+	var car_model string = "mustang"
+	var car_number int = 1992
+	// tip: when you declaration a variable like top, you should use it.
+	// go gives you an error for unsued variable
+
+	_ = car_number
+	// if you don't want to get compile error -> fist way
+	_, _ = car_name, car_model
+	// if you don't want to get compile error -> second way
+
+	// second way to declaration a variable
+	what_are_you_doing := "learning Golang"
+	fmt.Println("hello world", what_are_you_doing)
+}
